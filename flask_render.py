aac54b1391331a864df824db089723a9db9d2590
ianjuma <ian.juma@yahoo.com>
2014-02-03 21:09:00 +0300

    server port

--- flask_render.py
+++ flask_render.py
@@ -66,5 +66,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(port=8000, debug=True, host='0.0.0.0')
+    app.run(port=5000, debug=True, host='0.0.0.0')
     # This can be omitted if using gevent wrapped around gunicorn
